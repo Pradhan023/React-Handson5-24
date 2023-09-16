e9d999b51757cb4449e90ee5767137c0e8743782
Anish Pradhan <anishpradha5523@gmail.com>
2023-09-16 18:08:57 +0530

    Handson 5

--- src/Component/Purecomp.jsx
+++ src/Component/Purecomp.jsx
@@ -4,7 +4,7 @@ import '../Styling/Pure.css'
 class Pcomp extends PureComponent{
     state={
         Question : 'Q. What is Pure Compoent ?',
-        Ans : 'Pure Components in React are the components which do not re-renders when the value of state and props has bee updated with the same values.',
+        Ans : 'Pure Components in React are the components which do not re-renders when the value of state and props has been updated with the same values. So, This answer or explation line as expample of Pure Component to make it unique. here i am take button to re-render the state again but when you hit the button(Answer) value which is the the answer line dont get re-render again mean it perform invisible or visible function and again because of PureComponent method which is predefined react method. It prevent the re-rendering.   ',
         val: true,
         data:[]
     }
@@ -16,8 +16,10 @@ class Pcomp extends PureComponent{
         return(
             <div className="Pure-cont">
             <h1>{this.state.Question}</h1>
+            <div className="ans-fl">
             <button onClick={this.show}><span>Answer.</span></button>
             {this.state.val?'':<h3>{this.state.Ans}</h3>}
+            </div>
 
             </div>
         )
--- src/Display.jsx
+++ src/Display.jsx
@@ -6,7 +6,14 @@ const Display = (props) => {
     const Data = props.Data
   return (
     <div className='Hoc-Cont'>
-        <h1>Higher Order Component</h1>
+        <h1>Higher Order Component</h1><br/>
+        <pre>
+        <h1>Q. What is Hiher order Component?</h1><br/>
+        <h3>Ans. HOC is a function which takes a Wrapped component as input argument and returns a new Enhanced component. It should always be a pure function.</h3><br/>
+        <p>so, here in this example of Hoc i took the argument as input('props') which is a wrapped component and this wrapped component is passing all the data 
+            <br/>as props moreover following parent to child approach. In Hoc Component from where input is coming , there we are taking or writng function inside 
+            <br/>function and passing the enhance function pass props in wrapped.  Here i am invoking the Display function inside Hoc as argument.</p>
+        </pre>
         <table>
             <thead>
                 <tr>
